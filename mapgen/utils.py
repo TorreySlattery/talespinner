8060e882fce36f3723437fdd344b4e4ca8ebb40f
Torrey Slattery <torrey.slattery@gmail.com>
2016-07-03 21:02:34 -0700

    Stub in some basic functions

--- mapgen/utils.py
+++ mapgen/utils.py
@@ -1,10 +1,23 @@
 import random
 
-def get_feat_char(seed):
-    pass
+def get_feat_char(index):
+    if 0 < index < 10:
+        pass
+    elif 10 < index < 40:
+        pass
+    elif 40 < index < 95:
+        pass
+    elif 95 < index < 100:
+        pass
+    else:
+        return '?'
 
 def print_list_map(_list):
-    pass
+    for row in _list:
+        print(''.join(row))
 
-def generate():
-    pass
+def generate_maze():
+    width = 40
+    height = 20
+    map = [['#' for jj in range(height)] for ii in range(width)]
+    print_list_map(map)
