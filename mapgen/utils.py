142cc5c3cdc2c1a387f5d7d13555ee6e38582767
Torrey Slattery <torrey.slattery@gmail.com>
2016-07-24 19:56:56 -0700

    Get mazegen working with tests

--- mapgen/utils.py
+++ mapgen/utils.py
@@ -7,20 +7,24 @@ class Maze(object):
         self.width = kwargs.get('width', 40)
         self.height = kwargs.get('height', 20)
 
-        self.generate(**kwargs)
-
-        origin_x = random.randint(0, self.width)
-        origin_y = random.randint(0, self.height)
+        origin_x = random.randint(0, self.width-1)
+        origin_y = random.randint(0, self.height-1)
         self.cursor = (origin_x, origin_y)  # Set a start position
+
+        self.generate(**kwargs)
 
     def __str__(self):
         _str = ''
-        for row in self.maze:
+        upright = [row for row in reversed(self.maze)]
+        for row in upright:
             _str += ''.join(str(x) for x in row) + "\n"
         return _str
 
     def generate(self, **kwargs):
         self.maze = [[0 for jj in range(self.width)] for ii in range(self.height)]
+        x,y = self.cursor
+        self.maze[y][x] = 5
+        self.dig(self.cursor)
 
     def get_feat_char(self, index):
         if 0 < index < 10:
@@ -38,9 +42,31 @@ class Maze(object):
         x,y = curr
         directions = self.get_dirs()
         for direction in directions:
-            # Check if we can dig in that direction. If so, change the cell
-            # and then move the cursor to this position, then recurse
-            pass
+            dx,dy = direction
+
+            if x+dx < 0 or self.width <= x+dx:
+                continue
+            if y+dy < 0 or self.height <= y+dy:
+                continue
+
+            if self.maze[y+dy][x+dx]:
+                #Diggable cells are zeroes
+                continue
+            quad = self.get_quad((x+dx, y+dy))
+            occupied = False
+            for cardinal in quad:
+                cx,cy = cardinal
+                foo = (dx+cx, dy+cy)
+                if quad[cardinal] and foo != (0,0):
+                    occupied = True
+                    break
+
+            if occupied:
+                continue
+
+            # If we make it this far, our expected dig site is clear, so let's excavate and recurse
+            self.maze[y+dy][x+dx] = 1  # Sure, what the hell
+            self.dig((x+dx, y+dy))
 
     def get_dirs(self):
         directions = [(1,0), (-1, 0), (0, 1), (0,-1)]
@@ -53,11 +79,31 @@ class Maze(object):
         """
         x,y = point
 
-        return {
-                "N": self.maze[y-1][0] if y!= 0 else None,
-                "E": self.maze[y][x+1] if x < self.width-1 else None,
-                "S": self.maze[y+1][x] if y < self.height-1 else None,
-                "W": self.maze[y][x-1] if x!= 0 else None
-               }
+        quad = {
+                (0,1): None,
+                (1,0): None,
+                (0,-1): None,
+                (-1,0): None
+                }
+        try:
+            quad[(0,1)] = self.maze[y+1][x] if y < self.height-1 else None
+        except IndexError:
+            pass
 
+        try:
+            quad[(1,0)] = self.maze[y][x+1] if x < self.width else None
+        except IndexError:
+            pass
+
+        try:
+            quad[(0,-1)] = self.maze[y-1][x] if 0 < y else None
+        except IndexError:
+            pass
+
+        try:
+            quad[(-1,0)] = self.maze[y][x-1] if 0 < x else None
+        except IndexError:
+            pass
+
+        return quad
 
