0c886d56cfc6a74c5a484f0c95a6fab98368b4be
Torrey Slattery <torrey.slattery@gmail.com>
2016-08-20 17:56:48 -0700

    Stub in basic representation of a room

--- dungeon/views.py
+++ dungeon/views.py
@@ -5,3 +5,9 @@ class IndexView(TemplateView):
     template_name = 'dungeon/index.html'
 
     # We'll eventually want to pay attention to the url to load specific maps
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['room'] = [[0, 1, 1],
+                           [1, 0, 0],
+                           [1, 1, 1]]
+        return context
