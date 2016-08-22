62416d39913efb1fd5945bcdaa9594e72ff87240
Torrey Slattery <torrey.slattery@gmail.com>
2016-08-21 20:27:36 -0700

    Fix broken test

--- display/tests.py
+++ display/tests.py
@@ -1,11 +1,12 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
 
 from display.views import IndexView
 
 class IndexViewTestCase(TestCase):
 
     def test_smoke_test(self):
-        response = self.client.get(reverse('display:index'))
+        response = self.client.get(reverse('index'))
         self.assertEqual(response.status_code, 200)
 
     # We'll add more meaningful tests as the view and template get fleshed out
