e5a628bfc8c4b82ff66f91bb771a896c7ed722b8
Torrey Slattery <torrey.slattery@gmail.com>
2020-05-11 16:29:16 -0700

    Change project name to talespinner

--- talespinner/wsgi.py
+++ talespinner/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for compendium_5e_service project.
+WSGI config for talespinner project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "compendium_5e_service.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "talespinner.settings")
 
 application = get_wsgi_application()
