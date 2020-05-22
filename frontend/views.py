817ad8877a6ef48477b7376a969f396eb8d45a87
Torrey Slattery <torrey.slattery@gmail.com>
2020-05-22 15:15:26 -0700

    Add stub template and get route working

--- frontend/views.py
+++ frontend/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
-# Create your views here.
+"""
+In general, the idea here is to treat the frontend as if it were an external client that has to consume the 
+endpoint response like anyone else would.  
+"""
+
+
+class EncounterView(TemplateView):
+    template_name = "frontend/encounters.html"
+
+    def get_context_data(self, **kwargs):
+        return super().get_context_data()
--- talespinner/urls.py
+++ talespinner/urls.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [path("admin/", admin.site.urls), path("api/", include("api.urls"),)]
+from frontend.views import EncounterView
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/", include("api.urls"),),
+    path("encounters/", EncounterView.as_view(), name="encounters_list"),
+    path("encounters/<int:pk>/", EncounterView.as_view(), name="encounters_detail")
+]
