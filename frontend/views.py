6e91cfdaa90a7597c627512359ca26a32e51111f
Torrey Slattery <torrey.slattery@gmail.com>
2020-05-22 16:40:35 -0700

    Add basic request/response cycle

--- frontend/views.py
+++ frontend/views.py
@@ -1,13 +1,29 @@
-from django.views.generic.base import TemplateView
+import json
 
-"""
-In general, the idea here is to treat the frontend as if it were an external client that has to consume the 
-endpoint response like anyone else would.  
-"""
+import requests
+
+from django.views.generic.base import TemplateView
+from django.shortcuts import render
+
+from talespinner import constants
 
 
 class EncounterView(TemplateView):
+    """
+    In general, the idea here is to treat the frontend as if it were an external client that has to consume the
+    endpoint response like anyone else would, so no reverse() calls or direct model access, etc.
+    """
     template_name = "frontend/encounters.html"
 
-    def get_context_data(self, **kwargs):
-        return super().get_context_data()
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        encounter_id = context.get("pk", None)
+        url = f"{constants.TALESPINNER_BASE_URL}{constants.TALESPINNER_ENCOUNTER_URL}"
+        if encounter_id:
+            url = f"{url}{encounter_id}"
+
+        response = requests.get(url)
+        data = response.json()
+        context["data"] = data
+
+        return render(request, self.template_name, context)
---
+++ talespinner/constants.py
@@ -0,0 +1,2 @@
+TALESPINNER_BASE_URL = "http://localhost:8000/"
+TALESPINNER_ENCOUNTER_URL = "api/encounters/"
