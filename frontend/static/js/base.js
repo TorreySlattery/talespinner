2e450336a5b2f7c585708217f8d49b1a27bca481
Torrey Slattery <torrey.slattery@gmail.com>
2020-05-25 19:16:26 -0700

    Stub in roll for stats logic

--- frontend/static/js/base.js
+++ frontend/static/js/base.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
     $("img.roll-image").on('click', function(){
-        console.log("This is where we'd make a POST to the roll endpoint")
+        console.log("todo: add ajax call to roll endpoint")
+        var response = ["19+3=22", "7+3=10"]
+        var data_stat = $(this).attr('data-target')
+        var el = $("td[data-stat='" + data_stat + "']");
+        el.html(response[0] + "<br/>" + response[1])
     });
 })
