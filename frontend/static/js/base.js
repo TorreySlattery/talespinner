72260d21012a535af3e12c6e8bca11cb55440068
Torrey Slattery <torrey.slattery@gmail.com>
2020-05-27 11:36:16 -0700

    Switch to vertical table format

--- frontend/static/js/base.js
+++ frontend/static/js/base.js
@@ -1,18 +1,18 @@
 $(document).ready(function(){
     $("img.roll-image").on('click', function(){
-        var data_stat = $(this).attr('data-target');
+        var $rollOutputField = $(this).closest('td').siblings("td[data-roll-field]")
         var $parentTable = $(this).closest('table.table')
-        var data_creature_id = $parentTable.attr('data-creature-id')
+        var creature_id = $parentTable.attr('data-creature-id')
+        var field_name = $rollOutputField.attr("data-roll-field")
         $.ajax({
             type: "POST",
             url: "http://localhost:8000/api/roll/",
             headers: { "Authorization": "Token " + AUTH_TOKEN },
-            data: { "creature_id": data_creature_id, "field_name": data_stat},
+            data: { "creature_id": creature_id, "field_name": field_name},
         }).done(function(data){
             var first_roll = data["result"][0];
             var second_roll = data["result"][1];
-            var el = $parentTable.find("td[data-stat='" + data_stat + "']");
-            el.html(`<div class="roll-result">${first_roll}</div><div class="roll-result">${second_roll}</div>`);
+            $rollOutputField.html(`<div class="roll-result">${first_roll}</div><div class="roll-result">${second_roll}</div>`);
         }).fail(function(err){
             console.log(err);
         });
